c2307c666a66e1c67927d028f0118e16aa637f7e
Trey Hoover <hoovertr@flip1.engr.oregonstate.edu>
2018-02-10 19:32:14 -0800

    changes

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 function addTable{
-    var body = document.getElementsByTagName("body")[0];
+    var newBody = document.getElementsByTagName("body")[0];
 
     var newTable = document.createElement("table");
     var tableBody = document.createElement("tbody");
 
     for(var i = 0; i < 4; i++){
 	var newRow = document.createElement("tr");
-	if(i == 0){
-	    for(var j = 1; j < 5; j++){
+	for(var j = 0; j < 4; j++){
+	    if(i == 0){
 		var newHeader = document.createElement("th");
-		var headText = document.createTextNode("Header " + j);
+		var headText = document.createTextNode("Header "+(j));
 		newHeader.appendChild(headText);
 		newRow.appendChild(newHeader); 
 	    } 
-	}
-	else{
-	    for(var j = 1; j < 5; j++){
-		var newCell = document.createElement("td");
-		var cellText = document.createTextNode(j+" , "+i);
+	    else{
+	    	var newCell = document.createElement("td");
+		var cellText = document.createTextNode((j)+" , "+(i));
 		newCell.appendChild(cellText);
 		newRow.appendChild(newCell); 
 	    }
-        }
+        } 
+    
 	tableBody.appendChild(newRow);	
     }
     newTable.appendChild(tableBody);
-    body.appendChild(newTable);	 
+    newBody.appendChild(newTable);	 
 
 }
 
