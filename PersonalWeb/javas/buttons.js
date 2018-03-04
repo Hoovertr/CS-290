e408e2eed6b6defc4a189fb9a57c4c26695d8628
Trey Hoover <hoovertr@flip1.engr.oregonstate.edu>
2018-03-04 15:36:48 -0800

    Added some stuff

---
+++ PersonalWeb/javas/buttons.js
@@ -0,0 +1,16 @@
+document.getElementById("page1").addEventListener('click', function(event){
+	window.location.href = '/page1';
+	event.preventDefault();
+});
+
+
+document.getElementById("page2").addEventListener('click', function(event){
+	window.location.href = '/page2';
+	event.preventDefault();
+});
+
+
+document.getElementById("page3").addEventListener('click', function(event){
+	window.location.href = '/page3';
+	event.preventDefault();
+});
---
+++ PersonalWeb/javas/buttons1.js
@@ -0,0 +1,16 @@
+document.getElementById("home").addEventListener('click', function(event){
+	window.location.href = '/';
+	event.preventDefault();
+});
+
+
+document.getElementById("page2").addEventListener('click', function(event){
+	window.location.href = '/page2';
+	event.preventDefault();
+});
+
+
+document.getElementById("page3").addEventListener('click', function(event){
+	window.location.href = '/page3';
+	event.preventDefault();
+});
